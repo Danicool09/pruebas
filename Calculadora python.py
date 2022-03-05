89bfb3758a2fa7fb94a467b495716635beca096b
Daniel <danielhernan09122003@gmail.com>
2022-03-05 17:11:24 +0100

    Calculadora base

---
+++ Calculadora python.py
@@ -0,0 +1,13 @@
+print("Introduce 2 numeros para operar número: ")
+numero1=input("digite el numero 1:")
+numero2=input("digite el numero 2:")
+print("Selección de operación: ")
+print("1. Suma")
+print("2. Resta")
+opcion=input("tipo de operacion:")
+if opcion=="1":
+    print("La suma de los numeros es:",int(numero1)+int(numero2))
+elif opcion=="2":
+    print("La resta de los numeros es:",int(numero1)-int(numero2))
+else:
+    print("Opcion no valida")
